dc91018c06fe6eca20cb74e1e1204a68ae00c512
majed316 <quicky316@gmail.com>
2014-10-10 20:49:32 +0300

    initial commit

---
+++ inc/config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * config.php
+ * from this file you set the database connection parameteres.
+ */
+
+$host   = 'localhost';
+$dbname = 'linux';
+$user   = 'root';
+$pass   = '';
+
--- inc/db.inc.php
+++ inc/db.inc.php
@@ -3,10 +3,10 @@
 /*
  * Created by Majed ali
  */
-
+include 'config.php';
 try
 {
-    $pdo = new PDO('mysql:host=localhost;dbname=linux', 'root', '');
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->exec('SET NAMES "utf8"');
