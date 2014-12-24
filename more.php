3df90df69c1414c61c07c5b39f8cbb03d0d3bd43
abdulrahmanabdullah <nfs056@gmail.com>
2014-12-25 01:49:23 +0300

    write more.php code

---
+++ more.php
@@ -0,0 +1,13 @@
+<?php 
+include './inc/db.inc.php';
+if(isset($_GET['category_cat_id']) && !empty($_GET['category_cat_id'])){
+    $cat_id = $_GET['category_cat_id'];
+$sql = "SELECT * FROM command WHERE category_cat_id='$cat_id'";
+$query = $db->query($sql);
+$result = $query->fetchall();
+foreach ($result as $row ){
+    echo $row['command_name'].'<br>'; 
+}}
+?>
+
+
