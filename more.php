6fec611db4c549247a676fd868a6c980f4d800c0
majed316 <quicky316@gmail.com>
2014-12-26 16:29:20 +0300

    Update more.php

--- more.php
+++ more.php
@@ -1,7 +1,7 @@
 <?php 
 include './inc/db.inc.php';
 if(isset($_GET['category_cat_id']) && !empty($_GET['category_cat_id'])){
-    $cat_id = $_GET['category_cat_id'];
+    $cat_id = addslashes($_GET['category_cat_id']);
 $sql = "SELECT * FROM command WHERE category_cat_id='$cat_id'";
 $query = $db->query($sql);
 $result = $query->fetchall();
